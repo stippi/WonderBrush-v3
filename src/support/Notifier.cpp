bc02600f72c46001ce13d5dc8c0e9352ffda53eb
Ingo Weinhold <ingo_weinhold@gmx.de>
2012-10-19 13:38:53 +0200

    Debug.h: Fix printf() warnings on Linux

--- src/support/Notifier.cpp
+++ src/support/Notifier.cpp
@@ -29,7 +29,7 @@ Notifier::~Notifier()
 	if (fListeners.CountItems() > 0) {
 		char message[256];
 		Listener* o = (Listener*)fListeners.ItemAt(0);
-		sprintf(message, "Notifier::~Notifier() - %ld "
+		sprintf(message, "Notifier::~Notifier() - %" B_PRId32 " "
 						 "listeners still watching, first: %s\n",
 						 fListeners.CountItems(), typeid(*o).name());
 		debugger(message);
@@ -92,7 +92,7 @@ Notifier::SuspendNotifications(bool suspend)
 
 	if (fSuspended < 0) {
 		fprintf(stderr, "Notifier::SuspendNotifications(false) - "
-						"error: suspend level below zero: %ld\n", fSuspended);
+			"error: suspend level below zero: %" B_PRId32 "\n", fSuspended);
 		debugger("");
 		fSuspended = 0;
 	}
