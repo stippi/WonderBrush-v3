e23746f47b6ed1c70669d01298ab17b8ba3069ba
Ingo Weinhold <ingo_weinhold@gmx.de>
2012-10-20 14:06:06 +0200

    OS.h: Add more definitions

--- src/platform/qt/OS.h
+++ src/platform/qt/OS.h
@@ -41,6 +41,26 @@ void debugger(const char *message);
 
 bigtime_t	system_time(void);
 
+
+/* semaphore flags */
+enum {
+	B_CAN_INTERRUPT				= 0x01,	/* acquisition of the semaphore can be
+										   interrupted (system use only) */
+	B_CHECK_PERMISSION			= 0x04,	/* ownership will be checked (system use
+										   only) */
+	B_KILL_CAN_INTERRUPT		= 0x20,	/* acquisition of the semaphore can be
+										   interrupted by SIGKILL[THR], even
+										   if not B_CAN_INTERRUPT (system use
+										   only) */
+
+	/* release_sem_etc() only flags */
+	B_DO_NOT_RESCHEDULE			= 0x02	/* thread is not rescheduled */
+//	B_RELEASE_ALL				= 0x08,	/* all waiting threads will be woken up,
+//										   count will be zeroed */
+//	B_RELEASE_IF_WAITING_ONLY	= 0x10	/* release count only if there are any
+//										   threads waiting */
+};
+
 sem_id		create_sem(int32 count, const char* name);
 status_t	delete_sem(sem_id id);
 status_t	acquire_sem(sem_id id);
