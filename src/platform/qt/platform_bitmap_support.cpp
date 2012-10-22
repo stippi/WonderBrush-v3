66de6f1fc9490ace5d2ae8905674771bddd39fcd
Ingo Weinhold <ingo_weinhold@gmx.de>
2012-10-22 03:11:58 +0200

    Qt: scale_bitmap(): Call debugger()
    ... just to be sure we're not missing any use.

--- src/platform/qt/platform_bitmap_support.cpp
+++ src/platform/qt/platform_bitmap_support.cpp
@@ -1,10 +1,13 @@
 #include "bitmap_support.h"
 
+#include <OS.h>
+
 
 BBitmap*
 scale_bitmap(const BBitmap* bitmap, BRect newBounds)
 {
 // TODO:...
+debugger("scale_bitmap() not implemented yet");
 	return NULL;
 }
 
