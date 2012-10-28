1037131b66f3c4dab89959921cb421390531640c
Stephan Aßmus <superstippi@gmx.de>
2012-10-28 13:56:12 +0100

    Renamed FontManager to FontRegistry.  * This class originates from WonderBrush, was evolved in Clockwerk.    Now it is called FontRegistry, updated to compile.  * Initialize the FontRegistry (start font scanner thread) in    WonderBrush.

--- src/WonderBrush.cpp
+++ src/WonderBrush.cpp
@@ -13,6 +13,7 @@
 #include "Document.h"
 #include "Filter.h"
 #include "FontCache.h"
+#include "FontRegistry.h"
 #include "Image.h"
 #include "Layer.h"
 #include "Rect.h"
@@ -32,6 +33,9 @@ WonderBrushBase::WonderBrushBase(BRect bounds)
 	fWindowFrame(bounds.OffsetToCopy(50, 50)),
 	fWindowCount(0)
 {
+	// Init FontManager
+	FontRegistry::CreateDefault();
+
 	// create dummy contents for document
 	fDocument->RootLayer()->AddObject(new Rect(BRect(50, 100, 110, 130),
 		(rgb_color){ 255, 120, 0, 120 }));
