8cc8a2ea652cb6019ee0d3115807f975d3d226bb
Stephan Aßmus <superstippi@gmx.de>
2012-11-03 23:51:01 +0100

    Bring in line with the version from AGG currently in use.

--- src/render/text/FontEngine.cpp
+++ src/render/text/FontEngine.cpp
@@ -7,8 +7,8 @@
 // Anti-Grain Geometry - Version 2.4
 // Copyright (C) 2002-2005 Maxim Shemanarev (http://www.antigrain.com)
 //
-// Permission to copy, use, modify, sell and distribute this software 
-// is granted provided this copyright notice appears in all copies. 
+// Permission to copy, use, modify, sell and distribute this software
+// is granted provided this copyright notice appears in all copies.
 // This software is provided "as is" without express or implied
 // warranty, and with no claim as to its suitability for any purpose.
 //
@@ -50,7 +50,7 @@ dbl_to_int26p6(double p)
 template<class PathStorage>
 bool
 decompose_ft_outline(const FT_Outline& outline, bool flip_y, PathStorage& path)
-{   
+{
 	typedef typename PathStorage::value_type value_type;
 
 	FT_Vector v_last;
@@ -110,7 +110,7 @@ decompose_ft_outline(const FT_Outline& outline, bool flip_y, PathStorage& path)
 		x1 = int26p6_to_dbl(v_start.x);
 		y1 = int26p6_to_dbl(v_start.y);
 		if (flip_y) y1 = -y1;
-		path.move_to(value_type(dbl_to_int26p6(x1)), 
+		path.move_to(value_type(dbl_to_int26p6(x1)),
 					 value_type(dbl_to_int26p6(y1)));
 
 		while(point < limit) {
@@ -123,7 +123,7 @@ decompose_ft_outline(const FT_Outline& outline, bool flip_y, PathStorage& path)
 					x1 = int26p6_to_dbl(point->x);
 					y1 = int26p6_to_dbl(point->y);
 					if (flip_y) y1 = -y1;
-					path.line_to(value_type(dbl_to_int26p6(x1)), 
+					path.line_to(value_type(dbl_to_int26p6(x1)),
 								 value_type(dbl_to_int26p6(y1)));
 					//path.line_to(conv(point->x), flip_y ? -conv(point->y) : conv(point->y));
 					continue;
@@ -151,9 +151,9 @@ decompose_ft_outline(const FT_Outline& outline, bool flip_y, PathStorage& path)
 							x2 = int26p6_to_dbl(vec.x);
 							y2 = int26p6_to_dbl(vec.y);
 							if (flip_y) { y1 = -y1; y2 = -y2; }
-							path.curve3(value_type(dbl_to_int26p6(x1)), 
+							path.curve3(value_type(dbl_to_int26p6(x1)),
 										value_type(dbl_to_int26p6(y1)),
-										value_type(dbl_to_int26p6(x2)), 
+										value_type(dbl_to_int26p6(x2)),
 										value_type(dbl_to_int26p6(y2)));
 							continue;
 						}
@@ -169,14 +169,14 @@ decompose_ft_outline(const FT_Outline& outline, bool flip_y, PathStorage& path)
 						x2 = int26p6_to_dbl(v_middle.x);
 						y2 = int26p6_to_dbl(v_middle.y);
 						if (flip_y) { y1 = -y1; y2 = -y2; }
-						path.curve3(value_type(dbl_to_int26p6(x1)), 
+						path.curve3(value_type(dbl_to_int26p6(x1)),
 									value_type(dbl_to_int26p6(y1)),
-									value_type(dbl_to_int26p6(x2)), 
+									value_type(dbl_to_int26p6(x2)),
 									value_type(dbl_to_int26p6(y2)));
 
-						//path.curve3(conv(v_control.x), 
-						//			flip_y ? -conv(v_control.y) : conv(v_control.y), 
-						//			conv(v_middle.x), 
+						//path.curve3(conv(v_control.x),
+						//			flip_y ? -conv(v_control.y) : conv(v_control.y),
+						//			conv(v_middle.x),
 						//			flip_y ? -conv(v_middle.y) : conv(v_middle.y));
 
 						v_control = vec;
@@ -188,14 +188,14 @@ decompose_ft_outline(const FT_Outline& outline, bool flip_y, PathStorage& path)
 					x2 = int26p6_to_dbl(v_start.x);
 					y2 = int26p6_to_dbl(v_start.y);
 					if (flip_y) { y1 = -y1; y2 = -y2; }
-					path.curve3(value_type(dbl_to_int26p6(x1)), 
+					path.curve3(value_type(dbl_to_int26p6(x1)),
 								value_type(dbl_to_int26p6(y1)),
-								value_type(dbl_to_int26p6(x2)), 
+								value_type(dbl_to_int26p6(x2)),
 								value_type(dbl_to_int26p6(y2)));
 
-					//path.curve3(conv(v_control.x), 
-					//			flip_y ? -conv(v_control.y) : conv(v_control.y), 
-					//			conv(v_start.x), 
+					//path.curve3(conv(v_control.x),
+					//			flip_y ? -conv(v_control.y) : conv(v_control.y),
+					//			conv(v_start.x),
 					//			flip_y ? -conv(v_start.y) : conv(v_start.y));
 					goto Close;
 				}
@@ -206,9 +206,9 @@ decompose_ft_outline(const FT_Outline& outline, bool flip_y, PathStorage& path)
 					if (point + 1 > limit || FT_CURVE_TAG(tags[1]) != FT_CURVE_TAG_CUBIC)
 						return false;
 
-					vec1.x = point[0].x; 
+					vec1.x = point[0].x;
 					vec1.y = point[0].y;
-					vec2.x = point[1].x; 
+					vec2.x = point[1].x;
 					vec2.y = point[1].y;
 
 					point += 2;
@@ -227,18 +227,18 @@ decompose_ft_outline(const FT_Outline& outline, bool flip_y, PathStorage& path)
 						x3 = int26p6_to_dbl(vec.x);
 						y3 = int26p6_to_dbl(vec.y);
 						if (flip_y) { y1 = -y1; y2 = -y2; y3 = -y3; }
-						path.curve4(value_type(dbl_to_int26p6(x1)), 
+						path.curve4(value_type(dbl_to_int26p6(x1)),
 									value_type(dbl_to_int26p6(y1)),
-									value_type(dbl_to_int26p6(x2)), 
+									value_type(dbl_to_int26p6(x2)),
 									value_type(dbl_to_int26p6(y2)),
-									value_type(dbl_to_int26p6(x3)), 
+									value_type(dbl_to_int26p6(x3)),
 									value_type(dbl_to_int26p6(y3)));
 
-						//path.curve4(conv(vec1.x), 
-						//			flip_y ? -conv(vec1.y) : conv(vec1.y), 
-						//			conv(vec2.x), 
+						//path.curve4(conv(vec1.x),
+						//			flip_y ? -conv(vec1.y) : conv(vec1.y),
+						//			conv(vec2.x),
 						//			flip_y ? -conv(vec2.y) : conv(vec2.y),
-						//			conv(vec.x), 
+						//			conv(vec.x),
 						//			flip_y ? -conv(vec.y) : conv(vec.y));
 						continue;
 					}
@@ -250,18 +250,18 @@ decompose_ft_outline(const FT_Outline& outline, bool flip_y, PathStorage& path)
 					x3 = int26p6_to_dbl(v_start.x);
 					y3 = int26p6_to_dbl(v_start.y);
 					if (flip_y) { y1 = -y1; y2 = -y2; y3 = -y3; }
-					path.curve4(value_type(dbl_to_int26p6(x1)), 
+					path.curve4(value_type(dbl_to_int26p6(x1)),
 								value_type(dbl_to_int26p6(y1)),
-								value_type(dbl_to_int26p6(x2)), 
+								value_type(dbl_to_int26p6(x2)),
 								value_type(dbl_to_int26p6(y2)),
-								value_type(dbl_to_int26p6(x3)), 
+								value_type(dbl_to_int26p6(x3)),
 								value_type(dbl_to_int26p6(y3)));
 
-					//path.curve4(conv(vec1.x), 
-					//			flip_y ? -conv(vec1.y) : conv(vec1.y), 
-					//			conv(vec2.x), 
+					//path.curve4(conv(vec1.x),
+					//			flip_y ? -conv(vec1.y) : conv(vec1.y),
+					//			conv(vec2.x),
 					//			flip_y ? -conv(vec2.y) : conv(vec2.y),
-					//			conv(v_start.x), 
+					//			conv(v_start.x),
 					//			flip_y ? -conv(v_start.y) : conv(v_start.y));
 					goto Close;
 				}
@@ -271,7 +271,7 @@ decompose_ft_outline(const FT_Outline& outline, bool flip_y, PathStorage& path)
 		path.close_polygon();
 
    Close:
-		first = last + 1; 
+		first = last + 1;
 	}
 
 	return true;
@@ -401,7 +401,7 @@ FontEngine::Init(const char* fontFilePath, unsigned faceIndex, double size,
 
 	FT_Done_Face(fFace);
 	if (font_mem && font_mem_size) {
-		fLastError = FT_New_Memory_Face(fLibrary, (const FT_Byte*)font_mem, 
+		fLastError = FT_New_Memory_Face(fLibrary, (const FT_Byte*)font_mem,
 			font_mem_size, faceIndex, &fFace);
 	} else {
 		fLastError = FT_New_Face(fLibrary, fontFilePath, faceIndex, &fFace);
@@ -409,7 +409,7 @@ FontEngine::Init(const char* fontFilePath, unsigned faceIndex, double size,
 
 	if (fLastError != 0)
 		return false;
-		
+
 	switch(ren_type) {
 		case glyph_ren_native_mono:
 			fGlyphRendering = glyph_ren_native_mono;
@@ -453,8 +453,8 @@ FontEngine::PrepareGlyph(unsigned glyph_code)
 {
 	fGlyphIndex = FT_Get_Char_Index(fFace, glyph_code);
 	fLastError = FT_Load_Glyph(fFace, fGlyphIndex,
-		fHinting ? FT_LOAD_DEFAULT : FT_LOAD_NO_HINTING);
-//		fHinting ? FT_LOAD_FORCE_AUTOHINT : FT_LOAD_NO_HINTING);
+//		fHinting ? FT_LOAD_DEFAULT : FT_LOAD_NO_HINTING);
+		fHinting ? FT_LOAD_FORCE_AUTOHINT : FT_LOAD_NO_HINTING);
 
 	if (fLastError != 0)
 		return false;
@@ -463,49 +463,49 @@ FontEngine::PrepareGlyph(unsigned glyph_code)
 		case glyph_ren_native_mono:
 			fLastError = FT_Render_Glyph(fFace->glyph, FT_RENDER_MODE_MONO);
 			if (fLastError == 0) {
-				decompose_ft_bitmap_mono(fFace->glyph->bitmap, 
+				decompose_ft_bitmap_mono(fFace->glyph->bitmap,
 										 fFace->glyph->bitmap_left,
-										 kFlipY ? -fFace->glyph->bitmap_top : 
+										 kFlipY ? -fFace->glyph->bitmap_top :
 													 fFace->glyph->bitmap_top,
 										 kFlipY,
 										 fScanlineBin,
 										 fScanlineStorageBin);
 				fBounds.x1 = fScanlineStorageBin.min_x();
 				fBounds.y1 = fScanlineStorageBin.min_y();
-				fBounds.x2 = fScanlineStorageBin.max_x();
-				fBounds.y2 = fScanlineStorageBin.max_y();
-				fDataSize = fScanlineStorageBin.byte_size(); 
+				fBounds.x2 = fScanlineStorageBin.max_x() + 1;
+				fBounds.y2 = fScanlineStorageBin.max_y() + 1;
+				fDataSize = fScanlineStorageBin.byte_size();
 				fDataType = glyph_data_mono;
-				fAdvanceX = int26p6_to_dbl(fFace->glyph->advance.x);
-				fAdvanceY = int26p6_to_dbl(fFace->glyph->advance.y);
+				fAdvanceX = int16p16_to_dbl(fFace->glyph->linearHoriAdvance);
+				fAdvanceY = 0;//int16p16_to_dbl(fFace->glyph->linearVertAdvance);
 				return true;
 			}
 			break;
-	
-	
+
+
 		case glyph_ren_native_gray8:
 			fLastError = FT_Render_Glyph(fFace->glyph, FT_RENDER_MODE_NORMAL);
 			if (fLastError == 0) {
-				decompose_ft_bitmap_gray8(fFace->glyph->bitmap, 
+				decompose_ft_bitmap_gray8(fFace->glyph->bitmap,
 										  fFace->glyph->bitmap_left,
-										  kFlipY ? -fFace->glyph->bitmap_top : 
+										  kFlipY ? -fFace->glyph->bitmap_top :
 													  fFace->glyph->bitmap_top,
 										  kFlipY,
 										  fScanlineAA,
 										  fScanlineStorageAA);
 				fBounds.x1 = fScanlineStorageAA.min_x();
 				fBounds.y1 = fScanlineStorageAA.min_y();
-				fBounds.x2 = fScanlineStorageAA.max_x();
-				fBounds.y2 = fScanlineStorageAA.max_y();
-				fDataSize = fScanlineStorageAA.byte_size(); 
+				fBounds.x2 = fScanlineStorageAA.max_x() + 1;
+				fBounds.y2 = fScanlineStorageAA.max_y() + 1;
+				fDataSize = fScanlineStorageAA.byte_size();
 				fDataType = glyph_data_gray8;
-				fAdvanceX = int26p6_to_dbl(fFace->glyph->advance.x);
-				fAdvanceY = int26p6_to_dbl(fFace->glyph->advance.y);
+				fAdvanceX = int16p16_to_dbl(fFace->glyph->linearHoriAdvance);
+				fAdvanceY = 0;//int16p16_to_dbl(fFace->glyph->linearVertAdvance);
 				return true;
 			}
 			break;
-	
-	
+
+
 		case glyph_ren_outline:
 			fPath.remove_all();
 			if (decompose_ft_outline(fFace->glyph->outline, kFlipY,
@@ -518,8 +518,8 @@ FontEngine::PrepareGlyph(unsigned glyph_code)
 				fBounds.y1 = int(floor(bnd.y1));
 				fBounds.x2 = int(ceil(bnd.x2));
 				fBounds.y2 = int(ceil(bnd.y2));
-				fAdvanceX = int26p6_to_dbl(fFace->glyph->advance.x);
-				fAdvanceY = int26p6_to_dbl(fFace->glyph->advance.y);
+				fAdvanceX = int16p16_to_dbl(fFace->glyph->linearHoriAdvance);
+				fAdvanceY = 0;//int16p16_to_dbl(fFace->glyph->linearVertAdvance);
 				return true;
 			}
 			break;
@@ -533,21 +533,21 @@ FontEngine::PrepareGlyph(unsigned glyph_code)
 void
 FontEngine::WriteGlyphTo(uint8* data) const
 {
-	if (data && fDataSize) {
+	if (data != NULL && fDataSize) {
 		switch(fDataType) {
 			case glyph_data_mono:
 				fScanlineStorageBin.serialize(data);
 				break;
 
 			case glyph_data_gray8:
-				fScanlineStorageAA.serialize(data); 
+				fScanlineStorageAA.serialize(data);
 				break;
 
-			case glyph_data_outline: 
+			case glyph_data_outline:
 				fPath.serialize(data);
 				break;
 
-			case glyph_data_invalid: 
+			case glyph_data_invalid:
 			default:
 				break;
 		}
@@ -558,7 +558,7 @@ FontEngine::WriteGlyphTo(uint8* data) const
 bool
 FontEngine::GetKerning(unsigned first, unsigned second, double* x, double* y)
 {
-	if (fFace && first && second && FT_HAS_KERNING(fFace)) {
+	if (fFace != NULL && first != 0 && second != 0 && FT_HAS_KERNING(fFace)) {
 		FT_Vector delta;
 		FT_Get_Kerning(fFace, first, second, FT_KERNING_DEFAULT, &delta);
 
