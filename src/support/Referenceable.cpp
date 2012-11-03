ca844cc5ae37c55450c9ff66d45725c7cb9f8dda
Stephan Aßmus <superstippi@gmx.de>
2012-11-03 23:48:36 +0100

    Optional support for printing stack traces, disabled

--- src/support/Referenceable.cpp
+++ src/support/Referenceable.cpp
@@ -2,8 +2,22 @@
  * Copyright 2005-2007 Ingo Weinhold <ingo_weinhold@gmx.de>
  */
 
+#include <typeinfo>
+
+#include <dlfcn.h>
+#include <map>
+#include <stdlib.h>
+
+#include <Autolock.h>
+#include <image.h>
+#include <Locker.h>
+#include <String.h>
+
 #include "Debug.h"
 #include "Referenceable.h"
+#include "StackTrace.h"
+
+#define DEBUG_REFERENCES 0
 
 // constructor
 Referenceable::Referenceable(bool deleteWhenUnreferenced)
@@ -21,14 +35,33 @@ Referenceable::~Referenceable()
 void
 Referenceable::AddReference()
 {
+#if DEBUG_REFERENCES
+	int32 count = atomic_add(&fReferenceCount, 1);
+	BAutolock _(get_stack_trace_locker());
+	printf("[%ld] %p->AddReference(): %ld (%s)\n", find_thread(NULL), this,
+		count + 1, typeid(*this).name());
+	print_stack_trace();
+#else
 	atomic_add(&fReferenceCount, 1);
+#endif
 }
 
 // RemoveReference
 bool
 Referenceable::RemoveReference()
 {
+#if DEBUG_REFERENCES
+	int32 oldCount = atomic_add(&fReferenceCount, -1);
+	{
+		BAutolock _(get_stack_trace_locker());
+		printf("[%ld] %p->RemoveReference(): %ld (%s)\n", find_thread(NULL),
+			this, oldCount - 1, typeid(*this).name());
+		print_stack_trace();
+	}
+	bool unreferenced = oldCount == 1;
+#else
 	bool unreferenced = (atomic_add(&fReferenceCount, -1) == 1);
+#endif
 	if (fDeleteWhenUnreferenced && unreferenced)
 		delete this;
 	return unreferenced;
