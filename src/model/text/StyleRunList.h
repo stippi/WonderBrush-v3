142ef0a7c75542bbc224551b7fedfd1562984022
Stephan Aßmus <superstippi@gmx.de>
2012-11-05 08:29:51 +0100

    Added methods to extract and insert another StyleRunList as well as operators

--- src/model/text/StyleRunList.h
+++ src/model/text/StyleRunList.h
@@ -12,13 +12,20 @@ class StyleRun;
 class StyleRunList {
 public:
 								StyleRunList();
+								StyleRunList(const StyleRunList& other);
 	virtual						~StyleRunList();
+
+				StyleRunList&	operator=(const StyleRunList& other);
+				bool			operator==(const StyleRunList& other) const;
+				bool			operator!=(const StyleRunList& other) const;
 
 				void			MakeEmpty();
 
 				bool			Append(const StyleRun& runToAppend);
 				bool			Insert(int32 textOffset,
 									const StyleRun& runToInsert);
+				bool			Insert(int32 textOffset,
+									const StyleRunList& runListToInsert);
 
 				void			Remove(int32 textOffset, int32 length);
 
@@ -26,6 +33,10 @@ public:
 				const StyleRun*	StyleRunAt(int32 index) const;
 
 				int32			CountRuns() const;
+				int32			GetLength() const;
+
+				StyleRunList*	GetSubList(int32 textOffset,
+									int32 length) const;
 
 private:
 
