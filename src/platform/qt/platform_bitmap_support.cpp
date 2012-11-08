42e38d01ac870f23fde1a1815689f1848b9dd2c1
Ingo Weinhold <ingo_weinhold@gmx.de>
2012-11-08 16:05:25 +0100

    Qt: Implement scale_bitmap

--- src/platform/qt/platform_bitmap_support.cpp
+++ src/platform/qt/platform_bitmap_support.cpp
@@ -1,13 +1,24 @@
 #include "bitmap_support.h"
 
-#include <OS.h>
+#include <new>
+
+#include <Bitmap.h>
 
 
 BBitmap*
 scale_bitmap(const BBitmap* bitmap, BRect newBounds)
 {
-// TODO:...
-debugger("scale_bitmap() not implemented yet");
-	return NULL;
+	if (bitmap == NULL || bitmap->GetQImage() == NULL)
+		return NULL;
+
+	QImage scaledImage = bitmap->GetQImage()->scaled(
+		newBounds.IntegerWidth() + 1, newBounds.IntegerHeight() + 1,
+		Qt::IgnoreAspectRatio, Qt::SmoothTransformation);
+	BBitmap* scaledBitmap = new(std::nothrow) BBitmap(scaledImage);
+	if (scaledBitmap == NULL || !scaledBitmap->IsValid()) {
+		delete scaledBitmap;
+		return NULL;
+	}
+	return scaledBitmap;
 }
 
