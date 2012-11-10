4818b06b367cf49e26849eca4306b5198c0fbe7e
Ingo Weinhold <ingo_weinhold@gmx.de>
2012-11-10 13:11:34 +0100

    PlatformMixin: Templatize constructors

--- src/platform/qt/PlatformViewMixin.h
+++ src/platform/qt/PlatformViewMixin.h
@@ -50,16 +50,30 @@ private:
 template<typename BaseClass>
 class PlatformViewMixin : public BaseClass {
 public:
-	PlatformViewMixin(const char* name, uint32 flags)
+	template<typename Parameter1, typename Parameter2>
+	PlatformViewMixin(Parameter1 parameter1, Parameter2 parameter2)
 		:
-		BaseClass(name, flags)
+		BaseClass(parameter1, parameter2)
 	{
 	}
 
-	PlatformViewMixin(BRect frame, const char* name,
-		uint32 resizeMask, uint32 flags)
+	template<typename Parameter1, typename Parameter2, typename Parameter3,
+		typename Parameter4>
+	PlatformViewMixin(Parameter1 parameter1, Parameter2 parameter2,
+		Parameter3 parameter3, Parameter4 parameter4)
 		:
-		BaseClass(frame, name, resizeMask, flags)
+		BaseClass(parameter1, parameter2, parameter3, parameter4)
+	{
+	}
+
+	template<typename Parameter1, typename Parameter2, typename Parameter3,
+		typename Parameter4, typename Parameter5, typename Parameter6>
+	PlatformViewMixin(Parameter1 parameter1, Parameter2 parameter2,
+		Parameter3 parameter3, Parameter4 parameter4, Parameter5 parameter5,
+		Parameter6 parameter6)
+		:
+		BaseClass(parameter1, parameter2, parameter3, parameter4, parameter5,
+			parameter6)
 	{
 	}
 
