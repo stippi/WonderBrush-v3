874d061ca876d3f45a9d18dc320d9b245928ba7b
Stephan Aßmus <superstippi@gmx.de>
2012-11-11 10:16:03 +0100

    TextToolState uses Commands for the rest of the Text modifications  * Also sets the Style color when adopting the color at the caret    offset. Fixes appending new text at the end with the correct color.

--- src/tools/text/TextToolState.cpp
+++ src/tools/text/TextToolState.cpp
@@ -27,6 +27,7 @@
 #include "ObjectAddedCommand.h"
 #include "RemoveTextCommand.h"
 #include "support.h"
+#include "SetTextStyleCommand.h"
 #include "Text.h"
 #include "ui_defines.h"
 
@@ -794,8 +795,8 @@ TextToolState::SetFont(const char* family, const char* style)
 	fFontFamily = family;
 	fFontStyle = style;
 	if (_HasSelection()) {
-		fText->SetFont(_SelectionStart(), _SelectionLength(),
-			family, style);
+		View()->PerformCommand(new(std::nothrow) SetTextStyleCommand(
+			fText, _SelectionStart(), _SelectionLength(), family, style));
 		UpdateBounds();
 	}
 }
@@ -806,7 +807,8 @@ TextToolState::SetSize(float size)
 {
 	fSize = size;
 	if (_HasSelection()) {
-		fText->SetSize(_SelectionStart(), _SelectionLength(), size);
+		View()->PerformCommand(new(std::nothrow) SetTextStyleCommand(
+			fText, _SelectionStart(), _SelectionLength(), size));
 		UpdateBounds();
 	}
 }
@@ -825,15 +827,11 @@ TextToolState::SetWidth(float width)
 void
 TextToolState::SetColor(const rgb_color& color)
 {
-	::Style* style = new(std::nothrow) ::Style();
-	if (style == NULL)
-		return;
-
-	style->SetFillPaint(Paint(color));
-	fStyle.SetTo(style, true);
+	_SetStyle(color);
 
 	if (_HasSelection()) {
-		fText->SetColor(_SelectionStart(), _SelectionLength(), color);
+		View()->PerformCommand(new(std::nothrow) SetTextStyleCommand(
+			fText, _SelectionStart(), _SelectionLength(), color));
 		UpdateBounds();
 	}
 }
@@ -1266,11 +1264,24 @@ TextToolState::_AdoptStyleAtOffset(int32 textOffset)
 	color.green = RenderEngine::LinearToGamma(fgColor.g);
 	color.blue = RenderEngine::LinearToGamma(fgColor.b);
 	color.alpha = fgColor.a >> 8;
-	
+
 	fIgnoreColorColorNotifiactions = true;
 	fCurrentColor->SetColor(color);
 	fIgnoreColorColorNotifiactions = false;
+
+	_SetStyle(color);
 }
 
+// _SetStyle
+void
+TextToolState::_SetStyle(const rgb_color& color)
+{
+	::Style* style = new(std::nothrow) ::Style();
+	if (style == NULL)
+		return;
+
+	style->SetFillPaint(Paint(color));
+	fStyle.SetTo(style, true);
+}
 
 #endif	// _TEXTTOOLSTATE_CPP_
