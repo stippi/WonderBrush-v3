c51777f8298da24df773eec49a3ac035333722b2
Stephan Aßmus <superstippi@gmx.de>
2012-12-28 15:33:29 +0100

    Added functionality to stroke a Path via Path::Iterator interface

--- src/tools/path/haiku/PathToolStatePlatformDelegate.h
+++ src/tools/path/haiku/PathToolStatePlatformDelegate.h
@@ -44,6 +44,68 @@ public:
 			widthOffset.x + size + 1, widthOffset.y + size + 1));
 	}
 
+	void DrawPath(const Path& path, PlatformDrawContext& drawContext,
+		TransformViewState& viewState, float zoomLevel)
+	{
+		class StrokePathIterator : public Path::Iterator {
+		public:
+			StrokePathIterator(TransformViewState& viewState, BView* view)
+				: fViewState(viewState)
+				, fView(view)
+			{
+				fView->SetHighColor(0, 0, 0, 255);
+				fView->SetDrawingMode(B_OP_OVER);
+			}
+
+			virtual ~StrokePathIterator()
+			{
+			}
+
+			virtual void MoveTo(BPoint point)
+			{
+				fBlack = true;
+				fSkip = false;
+				fView->SetHighColor(0, 0, 0, 255);
+
+				fViewState.TransformObjectToView(&point, false);
+				fView->MovePenTo(point);
+			}
+
+			virtual void LineTo(BPoint point)
+			{
+				fViewState.TransformObjectToView(&point, false);
+				if (!fSkip) {
+					if (fBlack)
+						fView->SetHighColor(255, 255, 255, 255);
+					else
+						fView->SetHighColor(0, 0, 0, 255);
+					fBlack = !fBlack;
+
+					fView->StrokeLine(point);
+				} else {
+					fView->MovePenTo(point);
+				}
+				fSkip = !fSkip;
+			}
+
+		private:
+			TransformViewState&		fViewState;
+			BView*					fView;
+			bool					fBlack;
+			bool					fSkip;
+		};
+
+		BView* view = drawContext.View();
+
+		uint32 flags = view->Flags();
+		view->SetFlags(flags | B_SUBPIXEL_PRECISE);
+
+		StrokePathIterator iterator(viewState, view);
+		path.Iterate(&iterator, zoomLevel);
+
+		view->SetFlags(flags);
+	}
+
 	void DrawInvertedShape(PlatformDrawContext& drawContext, BShape& shape)
 	{
 		BView* view = drawContext.View();
