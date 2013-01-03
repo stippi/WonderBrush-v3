d87937835b46908d299e0ef321c256906f83fd2e
Stephan Aßmus <superstippi@gmx.de>
2013-01-03 22:12:56 +0100

    Fixed build (untracked file)

---
+++ src/edits/SetTextAlignmentEdit.h
@@ -0,0 +1,72 @@
+/*
+ * Copyright 2013, Stephan Aßmus <superstippi@gmx.de>.
+ * All rights reserved. Distributed under the terms of the MIT License.
+ */
+#ifndef SET_TEXT_ALIGNMENT_EDIT_H
+#define SET_TEXT_ALIGNMENT_EDIT_H
+
+#include <String.h>
+
+#include "UndoableEdit.h"
+#include "Text.h"
+
+class SetTextAlignmentEdit : public UndoableEdit {
+public:
+	SetTextAlignmentEdit(Text* text, uint32 alignment)
+		: UndoableEdit()
+		, fText(text)
+		, fAlignment(alignment)
+	{
+		fText->AddReference();
+	}
+
+	virtual ~SetTextAlignmentEdit()
+	{
+		fText->RemoveReference();
+	}
+
+	virtual	status_t InitCheck()
+	{
+		return fText->Alignment() != fAlignment ? B_OK : B_ERROR;
+	}
+
+	virtual	status_t Perform()
+	{
+		uint32 previousAlignment = fText->Alignment();
+		fText->SetAlignment(fAlignment);
+		fAlignment = previousAlignment;
+
+		return B_OK;
+	}
+
+	virtual status_t Undo()
+	{
+		return Perform();
+	}
+
+	virtual void GetName(BString& name)
+	{
+		name << "Change text alignment";
+	}
+
+	virtual	bool CombineWithNext(const UndoableEdit* _next)
+	{
+		const SetTextAlignmentEdit* next
+			= dynamic_cast<const SetTextAlignmentEdit*>(_next);
+
+		if (next == NULL || next->fText != fText
+			|| next->fTimeStamp - fTimeStamp > 500000) {
+			return false;
+		}
+
+		fTimeStamp = next->fTimeStamp;
+
+		return true;
+	}
+
+private:
+			Text*				fText;
+			uint32				fAlignment;
+};
+
+#endif // SET_TEXT_ALIGNMENT_EDIT_H
