dfff8add1123497166233a6b1c82f0bd1c442b6e
superstippi@gmx.de <stippi@shredder.(none)>
2013-01-06 14:22:34 +0000

    Added setters which return CharacterStyle instances with the modified properties.

--- src/model/text/CharacterStyle.cpp
+++ src/model/text/CharacterStyle.cpp
@@ -6,9 +6,23 @@
 #include "CharacterStyle.h"
 
 // constructor
-CharacterStyle::CharacterStyle(const Font& font, const StyleRef& style)
+CharacterStyle::CharacterStyle(const Font& font, double glyphSpacing,
+		double fauxWeight, double fauxItalic, const StyleRef& style)
 	: fFont(font)
+	, fGlyphSpacing(glyphSpacing)
+	, fFauxWeight(fauxWeight)
+	, fFauxItalic(fauxItalic)
 	, fStyle(style)
+{
+}
+
+// constructor
+CharacterStyle::CharacterStyle(const CharacterStyle& other)
+	: fFont(other.fFont)
+	, fGlyphSpacing(other.fGlyphSpacing)
+	, fFauxWeight(other.fFauxWeight)
+	, fFauxItalic(other.fFauxItalic)
+	, fStyle(other.fStyle.Get())
 {
 }
 
@@ -16,7 +30,13 @@ CharacterStyle::CharacterStyle(const Font& font, const StyleRef& style)
 bool
 CharacterStyle::operator==(const CharacterStyle& other) const
 {
-	return fFont == other.fFont && *(fStyle.Get()) == *(other.fStyle.Get());
+	if (this == &other)
+		return true;
+	return fFont == other.fFont
+		&& fGlyphSpacing == other.fGlyphSpacing
+		&& fFauxWeight == other.fFauxWeight
+		&& fFauxItalic == other.fFauxItalic
+		&& *(fStyle.Get()) == *(other.fStyle.Get());
 }
 
 // !=
@@ -25,3 +45,49 @@ CharacterStyle::operator!=(const CharacterStyle& other) const
 {
 	return *this != other;
 }
+
+// SetFont
+CharacterStyle
+CharacterStyle::SetFont(const Font& font) const
+{
+	CharacterStyle style(*this);
+	style.fFont = font;
+	return style;
+}
+
+// SetGlyphSpacing
+CharacterStyle
+CharacterStyle::SetGlyphSpacing(double spacing) const
+{
+	CharacterStyle style(*this);
+	style.fGlyphSpacing = spacing;
+	return style;
+}
+
+// SetFauxWeight
+CharacterStyle
+CharacterStyle::SetFauxWeight(double fauxWeight) const
+{
+	CharacterStyle style(*this);
+	style.fFauxWeight = fauxWeight;
+	return style;
+}
+
+// SetFauxItalic
+CharacterStyle
+CharacterStyle::SetFauxItalic(double fauxItalic) const
+{
+	CharacterStyle style(*this);
+	style.fFauxItalic = fauxItalic;
+	return style;
+}
+
+// SetStyle
+CharacterStyle
+CharacterStyle::SetStyle(const StyleRef& style) const
+{
+	CharacterStyle derived(*this);
+	derived.fStyle = style;
+	return derived;
+}
+
--- src/model/text/CharacterStyle.h
+++ src/model/text/CharacterStyle.h
@@ -11,21 +11,45 @@
 class CharacterStyle : public Referenceable {
 public:
 									CharacterStyle(const Font& font,
+										double glyphSpacing,
+										double fauxWeight, double fauxItalic,
 										const StyleRef& style);
 
-	inline	const Font&				GetFont() const
-										{ return fFont; }
-
-	inline	const StyleRef&			GetStyle() const
-										{ return fStyle; }
+									CharacterStyle(const CharacterStyle& other);
 
 			bool					operator==(
 										const CharacterStyle& other) const;
 
 			bool					operator!=(
 										const CharacterStyle& other) const;
+
+	// Getters
+	inline	const Font&				GetFont() const
+										{ return fFont; }
+
+	inline	double					GetGlyphSpacing() const
+										 { return fGlyphSpacing; }
+	inline	double					GetFauxWeight() const
+										 { return fFauxWeight; }
+	inline	double					GetFauxItalic() const
+										 { return fFauxItalic; }
+
+	inline	const StyleRef&			GetStyle() const
+										{ return fStyle; }
+
+	// Setters (return new instance with the changed property)
+			CharacterStyle			SetFont(const Font& font) const;
+			CharacterStyle			SetGlyphSpacing(double spacing) const;
+			CharacterStyle			SetFauxWeight(double fauxWeight) const;
+			CharacterStyle			SetFauxItalic(double fauxItalic) const;
+			CharacterStyle			SetStyle(const StyleRef& style) const;
+
+
 private:
 			Font					fFont;
+			double					fGlyphSpacing;
+			double					fFauxWeight;
+			double					fFauxItalic;
 			StyleRef				fStyle;
 };
 
