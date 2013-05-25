262f2b4ef75ffe42b7e9466048f8b68e74745434
Stephan Aßmus <superstippi@gmx.de>
2013-05-25 22:38:38 +0200

    PathToolState: Fixed copy & paste error.
     * Fixes DragPathPointState to have wrongly transformed
       drag origin when switching from the ToggleSmoothSharpState.

--- src/tools/path/PathToolState.cpp
+++ src/tools/path/PathToolState.cpp
@@ -365,7 +365,7 @@ public:
 			fParent->TransformObjectToCanvas(&originalOrigin);
 
 			fParent->SetDragState(fParent->fDragPathPointState);
-			fParent->fDragPathPointState->SetOrigin(fOrigin);
+			fParent->fDragPathPointState->SetOrigin(originalOrigin);
 			fParent->fDragPathPointState->DragTo(originalCurrent, modifiers);
 		}
 	}
