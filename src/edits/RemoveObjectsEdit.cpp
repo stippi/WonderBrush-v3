9e2fed7321c3202c65b225691ac2ef786bc84c9e
Stephan Aßmus <superstippi@gmx.de>
2013-05-26 12:27:26 +0200

    Implemented RemoveObjectsEdit.

---
+++ src/edits/RemoveObjectsEdit.cpp
@@ -0,0 +1,141 @@
+/*
+ * Copyright 2010-2013, Stephan Aßmus <superstippi@gmx.de>.
+ * All rights reserved.
+ */
+
+#include "RemoveObjectsEdit.h"
+
+#include <new>
+
+#include <stdio.h>
+
+#include "Layer.h"
+
+// constructor
+RemoveObjectsEdit::RemoveObjectsEdit(const ObjectList& objects,
+		Selection* selection)
+	: UndoableEdit()
+	, fObjects(objects)
+
+	, fOldPositions(new(std::nothrow) PositionInfo[objects.CountItems()])
+
+	, fSelection(selection)
+{
+//printf("RemoveObjectsEdit::RemoveObjectsEdit(%ld, %p, %ld)\n",
+//	objectCount, insertionLayer, insertionIndex);
+	if (fObjects.CountItems() == 0 || fOldPositions == NULL)
+		return;
+
+	// Remember current positions of all objects.
+	for (int32 i = 0; i < fObjects.CountItems(); i++) {
+		// Check if this object is a distant child of any of the other
+		// objects that we are moving and remove it from the array in that
+		// case.
+		bool ignoreObject = false;
+		for (int32 j = fObjects.CountItems() - 1; j >= 0; j--) {
+			if (j == i)
+				continue;
+			Layer* layer = dynamic_cast<Layer*>(fObjects.ItemAt(j).Get());
+			if (layer == NULL)
+				continue;
+			if (_ObjectIsDistantChildOf(fObjects.ItemAt(i).Get(), layer)) {
+//printf("ignoring %p\n", fObjects[i]);
+				ignoreObject = true;
+				break;
+			}
+		}
+		if (ignoreObject) {
+			fOldPositions[i].parent = NULL;
+		} else {
+			Layer* parent = fObjects.ItemAt(i)->Parent();
+			fOldPositions[i].parent = parent;
+			if (parent != NULL) {
+				fOldPositions[i].index = parent->IndexOf(
+					fObjects.ItemAt(i).Get());
+			} else {
+				fOldPositions[i].index = 0;
+			}
+		}
+	}
+}
+
+// destructor
+RemoveObjectsEdit::~RemoveObjectsEdit()
+{
+	delete[] fOldPositions;
+}
+
+// InitCheck
+status_t
+RemoveObjectsEdit::InitCheck()
+{
+	if (fObjects.CountItems() == 0 || fOldPositions == NULL)
+		return B_NO_INIT;
+
+	return B_OK;
+}
+
+// Perform
+status_t
+RemoveObjectsEdit::Perform()
+{
+	if (fSelection != NULL)
+		fSelection->DeselectAll(this);
+
+	for (int32 i = 0; i < fObjects.CountItems(); i++) {
+		if (fOldPositions[i].parent != NULL)
+			fOldPositions[i].parent->RemoveObject(fObjects.ItemAt(i).Get());
+	}
+
+	return B_OK;
+}
+
+// Undo
+status_t
+RemoveObjectsEdit::Undo()
+{
+	if (fSelection != NULL)
+		fSelection->DeselectAll(this);
+
+	for (int32 i = 0; i < fObjects.CountItems(); i++) {
+		Object* object = fObjects.ItemAt(i).Get();
+		if (fOldPositions[i].parent != NULL) {
+			fOldPositions[i].parent->AddObject(object,
+				fOldPositions[i].index);
+		}
+		if (fSelection != NULL)
+			fSelection->Select(Selectable(object), this, true);
+	}
+
+	return B_OK;
+}
+
+// GetName
+void
+RemoveObjectsEdit::GetName(BString& name)
+{
+	if (fObjects.CountItems() > 1)
+		name << "Remove objects";
+	else
+		name << "Remove object";
+}
+
+// #pragma mark -
+
+// _ObjectIsDistantChildOf
+bool
+RemoveObjectsEdit::_ObjectIsDistantChildOf(const Object* object,
+	const Layer* layer) const
+{
+	int32 count = layer->CountObjects();
+	for (int32 i = 0; i < count; i++) {
+		const Object* child = layer->ObjectAtFast(i);
+		if (object == child)
+			return true;
+		const Layer* subLayer = dynamic_cast<const Layer*>(child);
+		if (subLayer != NULL && _ObjectIsDistantChildOf(object, subLayer))
+			return true;
+
+	}
+	return false;
+}
---
+++ src/edits/RemoveObjectsEdit.h
@@ -0,0 +1,48 @@
+/*
+ * Copyright 2010-2013, Stephan Aßmus <superstippi@gmx.de>.
+ * All rights reserved.
+ */
+
+#ifndef REMOVE_OBJECTS_EDIT_H
+#define REMOVE_OBJECTS_EDIT_H
+
+#include "List.h"
+#include "UndoableEdit.h"
+#include "Selection.h"
+
+class Layer;
+class Object;
+
+class RemoveObjectsEdit : public UndoableEdit, public Selection::Controller {
+public:
+	typedef List<Reference<Object>, false, 1> ObjectList;
+
+public:
+								RemoveObjectsEdit(const ObjectList& objects,
+									Selection* selection);
+	virtual						~RemoveObjectsEdit();
+
+	virtual	status_t			InitCheck();
+
+	virtual	status_t			Perform();
+	virtual status_t			Undo();
+
+	virtual void				GetName(BString& name);
+
+private:
+			bool				_ObjectIsDistantChildOf(const Object* object,
+									const Layer* layer) const;
+
+private:
+			ObjectList			fObjects;
+
+			struct PositionInfo {
+				Layer*			parent;
+				int32			index;
+			};
+			PositionInfo*		fOldPositions;
+
+			Selection*			fSelection;
+};
+
+#endif // REMOVE_OBJECTS_EDIT_H
