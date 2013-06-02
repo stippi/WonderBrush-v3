8610ac6ae55e33e7e48a3b97b79dc09c1ff2642f
Stephan Aßmus <superstippi@gmx.de>
2013-06-02 10:27:31 +0200

    Added UndoableEdit for changing the fill paint of a style

---
+++ src/edits/StyleSetFillPaintEdit.h
@@ -0,0 +1,73 @@
+/*
+ * Copyright 2013, Stephan Aßmus <superstippi@gmx.de>.
+ * All rights reserved. Distributed under the terms of the MIT License.
+ */
+#ifndef STYLE_SET_FILL_PAINT_EDIT_H
+#define STYLE_SET_FILL_PAINT_EDIT_H
+
+#include <String.h>
+
+#include "Paint.h"
+#include "Style.h"
+#include "UndoableEdit.h"
+
+class StyleSetFillPaintEdit : public UndoableEdit {
+public:
+	StyleSetFillPaintEdit(Style* style, const Paint& newPaint)
+		: UndoableEdit()
+		, fStyle(style)
+		, fFillPaint(newPaint)
+	{
+	}
+
+	virtual ~StyleSetFillPaintEdit()
+	{
+	}
+
+	virtual	status_t InitCheck()
+	{
+		return fStyle.Get() != NULL
+			&& fStyle->FillPaint() != NULL
+			&& *fStyle->FillPaint() != fFillPaint ? B_OK : B_ERROR;
+	}
+
+	virtual	status_t Perform()
+	{
+		Paint oldPaint = *fStyle->FillPaint();
+		fStyle->SetFillPaint(fFillPaint);
+		fFillPaint = oldPaint;
+
+		return B_OK;
+	}
+
+	virtual status_t Undo()
+	{
+		return Perform();
+	}
+
+	virtual void GetName(BString& name)
+	{
+		name << "Change fill paint";
+	}
+
+	virtual	bool CombineWithNext(const UndoableEdit* _next)
+	{
+		const StyleSetFillPaintEdit* next
+			= dynamic_cast<const StyleSetFillPaintEdit*>(_next);
+
+		if (next == NULL || next->fStyle.Get() != fStyle.Get()
+			|| next->fTimeStamp - fTimeStamp > 500000) {
+			return false;
+		}
+
+		fTimeStamp = next->fTimeStamp;
+
+		return true;
+	}
+
+private:
+			StyleRef			fStyle;
+			Paint				fFillPaint;
+};
+
+#endif // STYLE_SET_FILL_PAINT_EDIT_H
