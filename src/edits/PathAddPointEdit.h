1cfec3c40a93fbc115f8ee468eb80d4a66be2364
Stephan Aßmus <superstippi@gmx.de>
2013-12-11 23:00:02 +0100

    ShapeEdit which adds a single point to a given Path

---
+++ src/edits/PathAddPointEdit.h
@@ -0,0 +1,61 @@
+/*
+ * Copyright 2013, Stephan Aßmus <superstippi@gmx.de>.
+ * All rights reserved. Distributed under the terms of the MIT License.
+ */
+#ifndef PATH_ADD_POINT_EDIT_H
+#define PATH_ADD_POINT_EDIT_H
+
+#include "ShapeEdit.h"
+#include "Path.h"
+
+class PathAddPointEdit : public ShapeEdit {
+public:
+	PathAddPointEdit(Shape* shape, const PathRef& path, BPoint point,
+			Selection* selection)
+		: ShapeEdit(shape, selection)
+		, fPath(path)
+		, fIndex(-1)
+		, fPoint(point)
+	{
+	}
+
+	virtual ~PathAddPointEdit()
+	{
+	}
+
+	virtual	status_t InitCheck()
+	{
+		return fPath.Get() != NULL ? B_OK : B_ERROR;
+	}
+
+	virtual	status_t Perform(EditContext& context)
+	{
+		SelectShape();
+		fIndex = fPath->CountPoints();
+		if (fPath->AddPoint(fPoint))
+			return B_OK;
+		else
+			return B_NO_MEMORY;
+	}
+
+	virtual status_t Undo(EditContext& context)
+	{
+		SelectShape();
+		if (fPath->RemovePoint(fIndex))
+			return B_OK;
+		else
+			return B_NO_MEMORY;
+	}
+
+	virtual void GetName(BString& name)
+	{
+		name << "Add control point";
+	}
+
+private:
+			PathRef				fPath;
+			int32				fIndex;
+			BPoint				fPoint;
+};
+
+#endif // REMOVE_TEXT_EDIT_H
