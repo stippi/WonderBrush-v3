fc4014d6179bbd77cc12c8fe8d95315e96cbbf1b
Stephan Aßmus <superstippi@gmx.de>
2015-04-18 18:10:44 +0200

    Definition for image interpolation modes.

---
+++ src/render/Interpolation.h
@@ -0,0 +1,17 @@
+/*
+ * Copyright 2015, Stephan Aßmus <superstippi@gmx.de>.
+ * All rights reserved. Distributed under the terms of the MIT license.
+ */
+
+#ifndef INTERPOLATION_H_
+#define INTERPOLATION_H_
+
+
+enum Interpolation {
+	INTERPOLATION_NEAREST_NEIGHBOR		= 0,
+	INTERPOLATION_BILINEAR				= 1,
+	INTERPOLATION_RESAMPLE				= 2,
+};
+
+
+#endif /* INTERPOLATION_H_ */
